f5bdee8ea89588b6076d7ec3a4d9656ea4c55acc
madao33 <3282389858@qq.com>
2022-04-06 11:34:47 +0800

    finish leetcode basic day 1

--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
@@ -1,24 +1,34 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int leftIdx = binarySearch(nums, target, true);
-        int rightIdx = binarySearch(nums, target, false) - 1;
-        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
-            return new int[]{leftIdx, rightIdx};
-        }
-        return new int[]{-1, -1};
-    }
-
-    public int binarySearch(int[] nums, int target, boolean lower) {
-        int left = 0, right = nums.length - 1, ans = nums.length;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > target || (lower && nums[mid] >= target)) {
-                right = mid - 1;
-                ans = mid;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length, m = matrix[0].length;
+        int l = -1, r = n - 1, row = 0;
+        // 查找是否存在行
+        while(l < r) {
+            int mid = (r - l + 1) / 2 + l;
+            if (matrix[mid][0] > target) {
+                r = mid - 1;
             } else {
-                left = mid + 1;
+                l = mid;
             }
         }
-        return ans;
+        row = l;
+        if (row < 0)
+            return false;
+
+        // 查找列
+        l = 0;
+        r = m - 1;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if (matrix[row][mid] == target)
+                return true;
+            if (matrix[row][mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return false;
+
     }
 }
--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
@@ -2,10 +2,9 @@ import java.util.List;
 
 public class mainTest {
     public static void main(String[] args){
-        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
-        int[] res = new Solution().searchRange(nums, 8);
-        for (int r : res)
-            System.out.print(r + "\t");
+        int[][] nums = new int[][]{{1}};
+        boolean res = new Solution().searchMatrix(nums, 0);
+        System.out.println(res);
 
     }
 }
