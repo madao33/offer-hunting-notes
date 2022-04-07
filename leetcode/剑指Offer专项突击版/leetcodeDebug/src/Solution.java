9b053b3609785df957cc8ea0e88691d07bde2d5e
madao33 <3282389858@qq.com>
2022-04-07 10:09:45 +0800

    update to day3

--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
-    public int findMin(int[] nums) {
-        int l = 0, r = nums.length - 1, res = Math.min(nums[r], nums[l]);
-        while(l < r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] >= nums[0])
-                l = mid + 1;
-            else
-                r = mid;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int target = -nums[i];
+            int l = i+1, r = n - 1;
+            while(l < r) {
+                if (nums[l] + nums[r] < target) {
+                    l++;
+                }else if(nums[l] + nums[r] > target) {
+                    r--;
+                }else {
+                    List<Integer> temp = new ArrayList<Integer>();
+                    temp.add(nums[i]);
+                    temp.add(nums[l]);
+                    temp.add(nums[r]);
+                    ans.add(temp);
+                    break;
+                }
+            }
         }
-        return Math.min(res, nums[l]);
+        return ans;
     }
 }
--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class mainTest {
     public static void main(String[] args){
-        int[] nums = new int[]{3, 1, 2};
-        int res = new Solution().findMin(nums);
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        List<List<Integer>> res = new Solution().threeSum(nums);
         System.out.println(res);
 
     }
