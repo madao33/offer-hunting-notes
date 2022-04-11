5fdfac4796fce527cba22ae1430593801351293f
madao33 <3282389858@qq.com>
2022-04-11 10:59:55 +0800

    update to day5

--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int maxArea(int[] height) {
-        int res = 0;
-        int l = 0, r = height.length - 1;
-        while(l < r) {
-            int h = Math.min(height[l], height[r]);
-            res = Math.max(res, h * (r-l));
-            if (height[l+1] < height[r-1])
-                r--;
-            else
-                l++;
+    public int minSubArrayLen(int target, int[] nums) {
+        int res = nums.length + 1, left = 0, acc = 0;
+        for (int right = 0; right < nums.length; right++) {
+            acc += nums[right];
+
+            while(acc >= target) {
+                res = Math.min(res, right - left + 1);
+                acc -= nums[left++];
+            }
         }
-        return res;
+
+        return res < nums.length + 1 ? res : 0;
     }
 }
--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
@@ -2,11 +2,9 @@ import java.util.List;
 
 public class mainTest {
     public static void main(String[] args){
-        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
-        int res = new Solution().maxArea(height);
+        int[] height = new int[]{1, 2, 3, 4, 5};
+        int res = new Solution().minSubArrayLen(15, height);
         System.out.println(res);
 
     }
 }
-
-
