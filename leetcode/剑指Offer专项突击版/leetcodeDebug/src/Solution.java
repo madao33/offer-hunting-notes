09c0d4aae1dc076285e623ae8cd3b35ab967bf86
madao33 <3282389858@qq.com>
2022-04-13 10:10:29 +0800

    finish day8

--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/Solution.java
@@ -1,24 +1,41 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    public Node connect(Node root) {
-        Queue<Node> queue = new LinkedList<Node>();
-        queue.add(root);
-        while(!queue.isEmpty()) {
-            Node pre = null;
-            for (int i = queue.size(); i > 0; i--) {
-                Node temp = queue.poll();
-                if (pre != null) {
-                    pre.next = temp;
 
-                }
-                temp.next = null;
-                pre = temp;
-                if (temp.left != null) queue.add(temp.left);
-                if (temp.right != null) queue.add(temp.right);
+    int[][] dirs = new int[][]{{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    public void solve(char[][] board) {
+        int n = board.length, m = board[0].length;
+
+        // 第一行和最后一行
+        for (int j = 0; j < m; j++) {
+            dfs(board, 0, j);
+            dfs(board, n-1, j);
+        }
+
+        // 第一列和最后一列
+        for (int i = 1; i < n-1; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, m-1);
+        }
+
+        // 不与边界连通的区域修改为'X'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j]=='A')
+                    board[i][j] = 'O';
+                else if (board[i][j]=='O')
+                    board[i][j] = 'X';
             }
         }
-        return root;
     }
+
+    public void dfs(char[][] board, int curi, int curj) {
+        int n = board.length, m = board[0].length;
+        // 越界或者不是'O'
+        if (curi < 0 || curi >= n || curj < 0 || curj >= m || board[curi][curj]!='O')
+            return ;
+        board[curi][curj] = 'A';
+        for (int[] dir : dirs) {
+            dfs(board, curi + dir[0], curj + dir[1]);
+        }
+    }
+
 }
--- leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
+++ leetcode/剑指Offer专项突击版/leetcodeDebug/src/mainTest.java
@@ -2,42 +2,10 @@ import java.util.List;
 
 public class mainTest {
     public static void main(String[] args){
-        Node root = new Node(3);
-        Node t2 = new Node(2);
-        root.left = t2;
-        Node t3 = new Node(3);
-        root.right = t3;
-        Node t4 = new Node(4);
-        t2.left = t4;
-        Node t5 = new Node(5);
-        t2.right = t5;
-        Node t7 = new Node(7);
-        t3.right = t7;
+        char[][] board = new char[][]{{'X','O','X','O','X','O'},{'O','X','O','X','O','X'},{'X','O','X','O','X','O'},{'O','X','O','X','O','X'}};
 
-
-        Node res = new Solution().connect(root);
-
-        System.out.println(res.val);
+        new Solution().solve(board);
+        System.out.println(board);
 
     }
 }
-
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
