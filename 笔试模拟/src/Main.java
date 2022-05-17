a20c9528433c66dbcbab3ea4ae7e512395ca2e99
madao33 <3282389858@qq.com>
2022-05-17 08:54:28 +0800

    first upload 字节2019春招

---
+++ 笔试模拟/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+
+    }
+}
+
+class feature{
+    int x, y;
+
+    public feature(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
