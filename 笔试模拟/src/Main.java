4cee25cb2652e1e19728f53d8ba91efc73172a5e
madao33 <3282389858@qq.com>
2022-06-06 16:58:40 +0800

    update 华为笔试

--- 笔试模拟/src/Main.java
+++ 笔试模拟/src/Main.java
@@ -1,38 +1,35 @@
 import java.util.*;
 
 public class Main {
+    public static Map<Character, Integer> map = new HashMap<>(){
+        {
+            put('0', 0);
+            put('1', 1);
+            put('2', 2);
+            put('3', 3);
+            put('4', 4);
+            put('5', 5);
+            put('6', 6);
+            put('7', 7);
+            put('8', 8);
+            put('9', 9);
+            put('A', 10);
+            put('B', 11);
+            put('C', 12);
+            put('D', 13);
+            put('E', 14);
+            put('F', 15);
+        }
+    };
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()) {
-            int n = scanner.nextInt();
-            int m = scanner.nextInt();
-            int cnt = 0;
-            for (int i = n; i <= m; i++) {
-                if (isWaterNum(i)) {
-                    if (cnt != 0)
-                        System.out.print(" ");
-                    System.out.print(i);
-                    cnt++;
-                }
-            }
-            if (cnt == 0)
-                System.out.println("no");
-            else
-                System.out.println();
+        String temp = scanner.nextLine();
+        char[] hexs = temp.toCharArray();
+        int res = 0;
+        for (int i = 2; i < hexs.length; i++) {
+            res = res * 16 + map.get(hexs[i]);
         }
-    }
-
-    public static boolean isWaterNum(int num) {
-        String str = Integer.toString(num);
-        char[] bits = str.toCharArray();
-        int sum = 0;
-        for (char bit : bits) {
-            int bit_num = bit - '0';
-            sum += bit_num * bit_num * bit_num;
-        }
-        if (sum == num)
-            return true;
-        return false;
+        System.out.println(res);
     }
 }
